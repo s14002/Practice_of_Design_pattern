4ab26c558a218df33de81658aac87e81be9931ac
s14002 <s14002@std.it-college.ac.jp>
2015-06-03 16:23:31 +0900

    2章おわり

--- src/jp/ac/it_college/std/s14002/pdp/Main.java
+++ src/jp/ac/it_college/std/s14002/pdp/Main.java
@@ -27,12 +27,27 @@ public class Main {
         // queueTest2();
         // stackTest();
         // hashSetTest1();
-        hashMapTest();
+        // hashMapTest();
+        practice1_1();
 
 
     }
 
     public static void IteratorMain() {
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("Around the World in 80 Days"));
+        bookShelf.appendBook(new Book("Bible"));
+        bookShelf.appendBook(new Book("Cinderella"));
+        bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+        Iterator it = bookShelf.iterator();
+
+        while (it.hasNext()) {
+            Book book = (Book)it.next();
+            System.out.println(book.getName());
+        }
+    }
+
+    public static void practice1_1() {
         BookShelf bookShelf = new BookShelf(4);
         bookShelf.appendBook(new Book("Around the World in 80 Days"));
         bookShelf.appendBook(new Book("Bible"));
